65f5d94ac2dd454612eea23608fb23bbea6aa00d
yxjay <yxjay@163.com>
2018-05-22 23:37:49 +0800

    Create coinInstance.py

---
+++ coinInstance.py
@@ -0,0 +1,11 @@
+class coinInstance(object):
+    def __init__():
+        pass
+    coinType     = None
+    rate         = 0.0
+    sellamount   = 0
+    buyamount    = 0
+    sell_count   = 0
+    sellList     = []
+    buy_count    = 0
+    buyList      = []
